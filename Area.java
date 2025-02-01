fd160aab12417f96daf879f0c834ebb34bc24562
Anurag1742 <anuragranjan373@gmail.com>
2025-02-01 19:48:41 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        final double PI = 3.14159;
+        
+        radius= 10.0;
+        
+        double area = PI * radius * radius;
+        System.out.println("The area of a circle with radius " + radius + " is " + area);
+    }
+}

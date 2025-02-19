f5e338ee54436b070b84cd5893f7de993ce78300
Anurag1742 <anuragranjan373@gmail.com>
2025-02-19 22:39:08 +0530

    Create J02MultiLineHello.java

---
+++ Basics_programs/J02MultiLineHello.java
@@ -0,0 +1,9 @@
+package Basics_programs;
+
+public class J02MultiLineHello {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}

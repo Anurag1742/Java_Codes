39d6740fdf2ca91e3e90627f537a2c64564f2abb
Anurag1742 <anuragranjan373@gmail.com>
2025-02-19 23:41:40 +0530

    Create J05AreaOfCircle.java

---
+++ Basics_programs/J05AreaOfCircle.java
@@ -0,0 +1,11 @@
+package Basics_programs;
+
+public class J05AreaOfCircle {
+    public static void main(String[] args) {
+        double radius;
+        final double PI = 3.14159;
+        radius = 10.0;
+        double area = PI * radius * radius;
+        System.out.println("Area of circle is " + area);
+    }
+}

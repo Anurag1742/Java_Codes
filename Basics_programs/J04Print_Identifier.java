571769b9657401ae2e7a5ecfafe3d6657a36dadc
Anurag1742 <anuragranjan373@gmail.com>
2025-02-19 23:26:48 +0530

    Create J04Print_Identifier.java

---
+++ Basics_programs/J04Print_Identifier.java
@@ -0,0 +1,15 @@
+package Basics_programs;
+
+public class J04Print_Identifier {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Anurag Ranjan";
+        System.out.println(name);
+
+        System.out.println(a + b);
+    }
+}

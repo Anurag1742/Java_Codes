c24df6bc6052c93ed04d3c089a8d43376a8de04d
Anurag1742 <anuragranjan373@gmail.com>
2025-02-19 23:09:25 +0530

    Create J03Pattern1.java

---
+++ Basics_programs/J03Pattern1.java
@@ -0,0 +1,10 @@
+package Basics_programs;
+
+public class J03Pattern1 {
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}

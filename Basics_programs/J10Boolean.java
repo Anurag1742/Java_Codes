3821674d59f5222fa2f7ea9dd622ffa4b1795595
Anurag1742 <anuragranjan373@gmail.com>
2025-02-20 20:39:27 +0530

    Create J10Boolean.java

---
+++ Basics_programs/J10Boolean.java
@@ -0,0 +1,16 @@
+package Basics_programs;
+
+public class J10Boolean {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+
+        if(b) {
+            System.out.println("This is executed. ");
+        }
+    }
+}

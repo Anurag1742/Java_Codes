869a44d34f45bdef35706af07d5290390df8d112
Anurag1742 <anuragranjan373@gmail.com>
2025-02-20 20:39:22 +0530

    Create J09Character.java

---
+++ Basics_programs/J09Character.java
@@ -0,0 +1,14 @@
+package Basics_programs;
+
+public class J09Character {
+    public static void main(String args[]) {
+        char aChar, bChar;
+        aChar = 88;
+        bChar = 'Y';
+        System.out.print("aChar and bChar: ");
+        System.out.println(aChar + " " + bChar);
+
+        aChar++;
+        System.out.println("aChar is now " + aChar);
+    }
+}

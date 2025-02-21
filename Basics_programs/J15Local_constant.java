22dbf6497b0e25d02003e6a369ae39f6d1a8f8b1
Anurag1742 <anuragranjan373@gmail.com>
2025-02-21 22:51:05 +0530

    Create J15Local_constant.java

---
+++ Basics_programs/J15Local_constant.java
@@ -0,0 +1,10 @@
+package Basics_programs;
+
+public class J15Local_constant {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        double r = 10.0, a;
+        a = PI * r * r;
+        System.out.println("Area of circle: " + a);
+    }
+}

d17db9238e1f8b1ccb6b5b35ffccad31587f14c3
Anurag1742 <anuragranjan373@gmail.com>
2025-02-21 22:51:11 +0530

    Create J16DeclareAtEnd.java

---
+++ Basics_programs/J16DeclareAtEnd.java
@@ -0,0 +1,9 @@
+package Basics_programs;
+
+public class J16DeclareAtEnd {
+    public static void main(String[] args) {
+        msg = "Hello Java Developer..! ";
+        System.out.println("Message: " + msg);
+    }
+    static String msg;
+}

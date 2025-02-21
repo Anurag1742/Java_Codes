a0a1391205391703d043be0ad3982778c0374f43
Anurag1742 <anuragranjan373@gmail.com>
2025-02-21 12:20:04 +0530

    Update J14Final_variable.java

---
+++ Basics_programs/J14Final_variable.java
@@ -0,0 +1,11 @@
+package Basics_programs;
+
+public class J14Final_variable {
+    final static double PI = 3.14159;
+    public static void main(String[] args) {
+        
+        double r = 10.0, a;
+        a = PI * r * r;
+        System.out.println("Area of circle: " + a);
+    }
+}

b288b5b42c4207bb6edc9e85eae828caa331138c
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:38:21 +0530

    Create J20Console.java

---
+++ Basics_programs/J20Console.java
@@ -0,0 +1,12 @@
+package Basics_programs;
+import java.io.*;
+
+public class J20Console {
+    public static void main(String[] args) {
+        Console cn = System.console();
+        int n;
+        System.out.print("Enter a number : ");
+        n = Integer.parseInt(cn.readLine());
+        System.out.println("The given number is : " + n);
+    }
+}

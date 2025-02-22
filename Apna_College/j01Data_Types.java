a391eba2f207c3077073287eff047135085a33ce
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:38:09 +0530

    Create j01Data_Types.java

---
+++ Apna_College/j01Data_Types.java
@@ -0,0 +1,22 @@
+package Apna_College;
+
+public class j01Data_Types {
+    public static void main(String args[]) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        float price = 10.5f;
+        System.out.println(price);
+        int number = 25;
+        System.out.println(number);
+        short n = 240;
+        System.out.println(n);
+        long l = 100000000000000000L;
+        System.out.println(l);
+        double d = 10.546445223336558556556;
+        System.out.println(d);
+    }
+}

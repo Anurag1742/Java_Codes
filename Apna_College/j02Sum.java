12c1b8a535b445c330c0914eef810fef36b7edbf
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:38:17 +0530

    Create j02Sum.java

---
+++ Apna_College/j02Sum.java
@@ -0,0 +1,10 @@
+package Apna_College;
+
+public class j02Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}

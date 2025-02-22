1bbab12bec06f6c3a7030051cf9af5fc0ad11d05
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:58:12 +0530

    Create J24ExitFunction.java

---
+++ Basics_programs/J24ExitFunction.java
@@ -0,0 +1,12 @@
+package Basics_programs;
+
+public class J24ExitFunction {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.print("Before exit. ");
+        if(a == 10)
+            System.exit(0);
+        System.out.println("After exit. ");
+    }
+}

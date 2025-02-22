4b9e024fd91e63c2f198eff743c76970d8710854
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:53:34 +0530

    Create J22Scanner.java

---
+++ Basics_programs/J22Scanner.java
@@ -0,0 +1,12 @@
+package Basics_programs;
+import java.util.Scanner;
+public class J22Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Enter a number : ");
+        n = sc.nextInt();
+        System.out.println("The given number is : " + n);
+
+    }
+}

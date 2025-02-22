e83c04ba22661b2b3ce8a8bd25a747b29e25a561
Anurag1742 <anuragranjan373@gmail.com>
2025-02-22 23:57:09 +0530

    Create J23Return.java

---
+++ Basics_programs/J23Return.java
@@ -0,0 +1,12 @@
+package Basics_programs;
+
+public class J23Return {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.print("Before return. ");
+        if(a == 10)
+            return;
+        System.out.println("After return. ");
+    }
+}

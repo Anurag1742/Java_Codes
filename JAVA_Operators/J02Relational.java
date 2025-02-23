e6557d6a0362b8ab911bd4c5a129a9686d12a72f
Anurag1742 <anuragranjan373@gmail.com>
2025-02-23 22:58:16 +0530

    Create J02Relational.java

---
+++ JAVA_Operators/J02Relational.java
@@ -0,0 +1,17 @@
+package JAVA_Operators;
+
+public class J02Relational {
+    public static void main(String[] args) {
+        int a = 20,b = 10;
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
+        System.out.println(" a > b : " + (a > b));
+        System.out.println(" a < b : " + (a < b));
+        System.out.println(" a >= b : " + (a >= b));
+        System.out.println(" a <= b : " + (a <= b));
+        System.out.println(" a == b : " + (a == b));
+        System.out.println(" a != b : " + (a != b));
+
+    }
+}

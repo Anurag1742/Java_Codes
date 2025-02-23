ef08afc2b3fb5bd7f1ecdb1d0f701c791ca10a46
Anurag1742 <anuragranjan373@gmail.com>
2025-02-23 23:13:40 +0530

    Create J08Conditional.java

---
+++ JAVA_Operators/J08Conditional.java
@@ -0,0 +1,12 @@
+package JAVA_Operators;
+
+public class J08Conditional {
+    public static void main(String[] args) {
+        int a, b, max;
+        a = 25;
+        b = 14;
+
+        max = ((a>b) ? a : b);
+        System.out.println("Maximum Value is : " + max);
+    }
+}

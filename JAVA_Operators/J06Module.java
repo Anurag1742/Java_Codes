c2200aea39f09977ecc8106bcde4b233635a4870
Anurag1742 <anuragranjan373@gmail.com>
2025-02-23 23:13:34 +0530

    Create J06Module.java

---
+++ JAVA_Operators/J06Module.java
@@ -0,0 +1,10 @@
+package JAVA_Operators;
+
+public class J06Module {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+        System.out.println("x mod 10 = " + x % 10); // % is a module operator
+        System.out.println("y mod 10 = " + y % 10); // % is a module operator
+    }
+}

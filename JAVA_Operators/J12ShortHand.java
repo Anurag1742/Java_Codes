c5459c7e6208ce6f8bb31067451f5cb3fbc15a87
Anurag1742 <anuragranjan373@gmail.com>
2025-02-24 23:37:06 +0530

    Create J12ShortHand.java

---
+++ JAVA_Operators/J12ShortHand.java
@@ -0,0 +1,26 @@
+package JAVA_Operators;
+
+public class J12ShortHand {
+    public static void main(String[] args) {
+        int a = 13;
+        System.out.println("a = " + a);
+
+        a &= 4;
+        System.out.println("a &= 4 : " + a);
+
+        a |= 3;
+        System.out.println("a |= 3 : " + a);
+
+        a ^= 8;
+        System.out.println("a ^= 8 : " + a);
+
+        a >>= 2;
+        System.out.println("a >>= 2 : " + a);
+
+        a <<= 3;
+        System.out.println("a <<= 3 : " + a);
+
+        a >>>= 3;
+        System.out.println("a >>>= 3 : " + a);
+    }
+}

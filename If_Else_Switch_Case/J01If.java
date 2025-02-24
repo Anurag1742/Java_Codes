57c49e4ccdf0d4dd3a63afebd493f74b75c5166a
Anurag1742 <anuragranjan373@gmail.com>
2025-02-24 23:51:02 +0530

    Create J01If.java

---
+++ If_Else_Switch_Case/J01If.java
@@ -0,0 +1,13 @@
+package If_Else_Switch_Case;
+
+public class J01If {
+    public static void main(String[] args) {
+        int n;
+
+        n = 230;
+
+        if(n > 100){
+            System.out.println("The number is greater than 100");
+        }
+    }
+}

7d22825c6d1aa93694f2b00384f87e8466916473
Anurag1742 <anuragranjan373@gmail.com>
2025-02-24 23:30:19 +0530

    Create J10LogicalAND.java

---
+++ JAVA_Operators/J10LogicalAND.java
@@ -0,0 +1,16 @@
+package JAVA_Operators;
+
+public class J10LogicalAND {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        if ((a < 5) && (++b < 10)) {
+            System.out.println("True");
+        }
+        System.out.println("b = " + b);
+
+        if((a < 5) && (++b > 10)) {
+            System.out.println("True");
+        }
+        System.out.println("b = " + ++b);
+    }
+}

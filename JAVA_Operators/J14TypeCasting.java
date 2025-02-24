6e18ab6c64e58ff81859bbb383a03f678da73f19
Anurag1742 <anuragranjan373@gmail.com>
2025-02-24 23:42:00 +0530

    Create J14TypeCasting.java

---
+++ JAVA_Operators/J14TypeCasting.java
@@ -0,0 +1,11 @@
+package JAVA_Operators;
+
+public class J14TypeCasting {
+    public static void main(String[] args) {
+        int a ;
+        float b = 123.333f;
+        System.out.println("Before Type Casting: " + b);
+        a = (int)b;
+        System.out.println("After Type Casting: " + a);
+    }
+}

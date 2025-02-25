6a6e8f0f0af2178dc2818d46665a4a0751723b37
Anurag1742 <anuragranjan373@gmail.com>
2025-02-25 23:42:45 +0530

    Create J06MaxInTwo.java

---
+++ If_Else_Switch_Case/J06MaxInTwo.java
@@ -0,0 +1,17 @@
+package If_Else_Switch_Case;
+
+public class J06MaxInTwo {
+    public static void main(String[] args) {
+        int a, b, max;
+
+        a = 23;
+        b = 14;
+
+        if (a>b){
+            max = a;
+        } else {
+            max = b;
+        }
+        System.out.println("\nMaximum : " + max);
+    }
+}

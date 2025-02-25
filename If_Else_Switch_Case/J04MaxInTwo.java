66dd2775565519a0034498d497bacab77e9b5966
Anurag1742 <anuragranjan373@gmail.com>
2025-02-25 23:35:35 +0530

    Create J04MaxInTwo.java

---
+++ If_Else_Switch_Case/J04MaxInTwo.java
@@ -0,0 +1,19 @@
+package If_Else_Switch_Case;
+import java.util.*;
+
+public class J04MaxInTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int a, b, c;
+        System.out.print("Enter First Number : ");
+        a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        b = sc.nextInt();
+
+        if (a>b) {
+            System.out.println("\nFirst Number is Greater Than Second Number.");
+        }else {
+            System.out.println("\nFirst Number is Less or Equal to Second Number.");
+        }
+    }
+}

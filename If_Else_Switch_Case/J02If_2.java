147156b13ba630043a2a2e956dac53ed1043b899
Anurag1742 <anuragranjan373@gmail.com>
2025-02-25 23:06:06 +0530

    Create J02If_2.java

---
+++ If_Else_Switch_Case/J02If_2.java
@@ -0,0 +1,19 @@
+package If_Else_Switch_Case;
+
+public class J02If_2 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } 
+
+        if (a < b) {
+            System.out.println("a is less than b");
+        }
+
+        if (a == b) {
+            System.out.println("a is equal to b");
+        }
+    }
+}

535f27ca4a43044d07ce8bf63fde44e3a8a64774
Anurag1742 <anuragranjan373@gmail.com>
2025-02-26 23:32:55 +0530

    Create J15DayFindWithSwitch.java

---
+++ If_Else_Switch_Case/J15DayFindWithSwitch.java
@@ -0,0 +1,35 @@
+package If_Else_Switch_Case;
+
+public class J15DayFindWithSwitch {
+    public static void main(String[] args) {
+        int day;
+        day = 6;
+
+        switch(day){
+            case 1:
+                System.out.println("Monday.");
+                break;
+            case 2:
+                System.out.println("Tuesday.");
+                break;
+            case 3:
+                System.out.println("Wednesday.");
+                break;
+            case 4:
+                System.out.println("Thursday.");
+                break;
+            case 5:
+                System.out.println("Friday.");
+                break;
+            case 6:
+                System.out.println("Satarday.");
+                break;
+            case 7:
+                System.out.println("Sunday.");
+                break;
+            default:
+                System.out.println("Wrong Day Number.");
+
+        }
+    }
+}

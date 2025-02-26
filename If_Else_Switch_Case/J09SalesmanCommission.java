27d62cbc5677d9bfc32175f868839073834a4321
Anurag1742 <anuragranjan373@gmail.com>
2025-02-26 23:08:40 +0530

    Create J09SalesmanCommission.java

---
+++ If_Else_Switch_Case/J09SalesmanCommission.java
@@ -0,0 +1,35 @@
+package If_Else_Switch_Case;
+
+public class J09SalesmanCommission {
+    public static void main(String[] args) {
+        double sales, comm;
+
+        sales =12000;
+
+        if(sales < 5000)
+        {
+            comm = 0;
+        }
+        else{
+            if(sales < 10000)
+            {
+                comm = sales * 0.05;
+            }
+            else{
+                if(sales < 20000)
+                {
+                    comm = sales * 0.1;
+                }
+                else {
+                    if(sales < 30000){
+                        comm = sales * 0.12;
+                    }
+                    else{
+                        comm = sales * 0.15;
+                    }
+                }
+            }
+        }
+        System.out.println("The commission is = " + comm);
+    }
+}

60aa757988f9b6e1b5974710cb26c539c19eb976
Anurag1742 <anuragranjan373@gmail.com>
2025-02-26 23:11:41 +0530

    Create J10IfElseLadder.java

---
+++ If_Else_Switch_Case/J10IfElseLadder.java
@@ -0,0 +1,20 @@
+package If_Else_Switch_Case;
+
+public class J10IfElseLadder {
+    public static void main(String[] args) {
+        int a = 3;
+
+        if (a == 1){
+            System.out.println("One");
+        }
+        else if (a == 2){
+            System.out.println("Two");
+        }
+        else if (a == 3){
+            System.out.println("Three");
+        }
+        else{
+            System.out.println("Invalid Value");
+        }
+    }
+}

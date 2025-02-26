2309bf8e5111796b4bd59a436a04fb6e47e4843b
Anurag1742 <anuragranjan373@gmail.com>
2025-02-26 23:18:55 +0530

    Create J12TypeOfNumber.java

---
+++ If_Else_Switch_Case/J12TypeOfNumber.java
@@ -0,0 +1,15 @@
+package If_Else_Switch_Case;
+
+public class J12TypeOfNumber {
+    public static void main(String[] args) {
+        int a = 13;
+
+        if(a < 0){
+            System.out.println("a is negative.");
+        }else if(a == 0){
+            System.out.println("a is zero.");
+        }else{
+            System.out.println("a is positive.");
+        }
+    }
+}

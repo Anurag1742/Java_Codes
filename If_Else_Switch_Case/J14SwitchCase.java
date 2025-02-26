aad6c04ce96f3401cbd11912f4d599ad50353090
Anurag1742 <anuragranjan373@gmail.com>
2025-02-26 23:28:56 +0530

    Create J14SwitchCase.java

---
+++ If_Else_Switch_Case/J14SwitchCase.java
@@ -0,0 +1,20 @@
+package If_Else_Switch_Case;
+
+public class J14SwitchCase {
+    public static void main(String[] args) {
+        int a = 2;
+       switch (a){
+        case 1 :
+            System.out.println("One");
+            break;
+        case 2 :
+            System.out.println("Two");
+            break;
+        case 3 :
+            System.out.println("Three");
+            break;
+        default:
+            System.out.println("Invalid value");
+       } 
+    }
+}

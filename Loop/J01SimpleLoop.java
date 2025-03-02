6edf0b4da92202435256a50e12e100af7ef040f3
Anurag1742 <anuragranjan373@gmail.com>
2025-03-02 22:59:37 +0530

    Create J01SimpleLoop.java

---
+++ Loop/J01SimpleLoop.java
@@ -0,0 +1,9 @@
+package Loop;
+
+public class J01SimpleLoop {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("Loop : " +i);
+        }
+    }
+}

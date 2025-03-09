d001de120f4757944e38cc5bc0975f3048c90454
Anurag1742 <anuragranjan373@gmail.com>
2025-03-09 23:58:32 +0530

    Create J01Static.java

---
+++ Arrays/J01Static.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class J01Static {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+        for (int i=0;i<5;i++){
+            System.out.println(a[i]);
+        }
+    }
+}

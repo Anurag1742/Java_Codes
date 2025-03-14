74e6fb524fde4d1bc71b16f2641c14cedd469e39
Anurag1742 <anuragranjan373@gmail.com>
2025-03-14 21:59:43 +0530

    Update J05SecondLargestNum.java

--- Arrays/J05SecondLargestNum.java
+++ Arrays/J05SecondLargestNum.java
@@ -36,3 +36,32 @@ public class J05SecondLargestNum {
     }
 
 }
+
+/*
+    Second largest number second method
+
+package Arrays;
+
+import java.util.*;
+
+public class J06ThirdLargestNum {
+    // Java program to find the third largest
+    // element in an array.
+
+    static int thirdLargest(int[] arr) {
+        int n = arr.length;
+
+        // Sort the array
+        Arrays.sort(arr);
+
+        // Return the third largest element
+        return arr[n - 2];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 14, 2, 16, 10, 20 };
+        System.out.println(thirdLargest(arr));
+    }
+}
+
+ */

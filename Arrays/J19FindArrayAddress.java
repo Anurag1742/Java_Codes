4824cd259d85df310dc3b4e07f204159bb66fdca
Anurag1742 <anuragranjan373@gmail.com>
2025-03-21 22:11:34 +0530

    Create J19FindArrayAddress.java

---
+++ Arrays/J19FindArrayAddress.java
@@ -0,0 +1,14 @@
+package Arrays;
+import java.util.*;
+
+public class J19FindArrayAddress {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}

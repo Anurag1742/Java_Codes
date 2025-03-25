d98635dd22f179ee999c8db2ad2d96af1da80aec
Anurag1742 <anuragranjan373@gmail.com>
2025-03-25 21:32:16 +0530

    Create FactorialRecursion.java

---
+++ FactorialRecursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialRecursion {
+
+    public static int Factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        sc.close();
+        System.out.println("Factorial of  " + num + " is  " + Factorial(num));
+    }
+}

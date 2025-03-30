8dcc7f8661e4b9f52a00ed750aacfa3b763bbfe3
Anurag1742 <anuragranjan373@gmail.com>
2025-03-30 22:57:48 +0530

    Create J02StringFunction.java

---
+++ Method_Function/J02StringFunction.java
@@ -0,0 +1,12 @@
+package Method_Function;
+
+public class J02StringFunction {
+    public static void main(String[] args) {
+        char ch[] = { 'J', 'A', 'V', 'A' };
+        String s = new String(ch, 2, 2);
+        System.out.println("String = " + s);
+
+        String s1 = "" + 2015;
+        System.out.println("String = " + s1);
+    }
+}

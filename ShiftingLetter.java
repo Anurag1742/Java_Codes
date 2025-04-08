f7f5589e037ef1ea5ef6f124cc5f757a793385a1
Anurag1742 <anuragranjan373@gmail.com>
2025-04-08 18:42:15 +0530

    Update ShiftingLetter.java

--- ShiftingLetter.java
+++ ShiftingLetter.java
@@ -3,13 +3,11 @@ public class ShiftingLetter {
         int n = s.length();
         int[] totalShifts = new int[n];
         
-        // Compute cumulative shifts from right to left
         totalShifts[n - 1] = shifts[n - 1] % 26;
         for (int i = n - 2; i >= 0; i--) {
             totalShifts[i] = (shifts[i] + totalShifts[i + 1]) % 26;
         }
         
-        // Apply shifts to characters
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
             char newChar = (char) (((s.charAt(i) - 'a' + totalShifts[i]) % 26) + 'a');

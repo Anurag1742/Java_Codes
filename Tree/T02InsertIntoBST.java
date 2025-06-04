39188b15bb56c6314b7e2039d5960404aac06c27
Anurag1742 <anuragranjan373@gmail.com>
2025-06-04 16:36:32 +0530

    Create T02InsertIntoBST.java

---
+++ Tree/T02InsertIntoBST.java
@@ -0,0 +1,57 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class T02InsertIntoBST {
+
+    public static TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.val + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Construct BST: [4,2,7,1,3]
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        int valToInsert = 5;
+
+        root = insertIntoBST(root, valToInsert);
+
+        System.out.print("Inorder traversal after insertion: ");
+        inorder(root);
+    }
+}
